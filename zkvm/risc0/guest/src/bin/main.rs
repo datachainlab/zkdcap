d6c9bad3621aeb133ce65a75769ee9913af5c5d8
Jun Kimura <jun.kimura@datachain.jp>
2025-02-22 21:17:53 +0900

    fix to handle both v3 and v4 quote in zkvm program
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- zkvm/risc0/guest/src/bin/main.rs
+++ zkvm/risc0/guest/src/bin/main.rs
@@ -1,16 +1,16 @@
 use dcap_quote_verifier::collaterals::IntelCollateral;
-use dcap_quote_verifier::types::quotes::version_3::QuoteV3;
-use dcap_quote_verifier::quotes::version_3::verify_quote_v3;
+use dcap_quote_verifier::types::quotes::Quote;
+use dcap_quote_verifier::quotes::verify_quote;
 use risc0_zkvm::guest::env;
 
 fn main() {
     let (quote, collaterals, current_time): (Vec<u8>, Vec<u8>, u64) = env::read();
 
-    let quote = QuoteV3::from_bytes(&quote).unwrap();
+    let quote = Quote::from_bytes(&quote).unwrap();
     let collaterals = IntelCollateral::from_bytes(&collaterals).unwrap();
 
     env::commit_slice(
-        verify_quote_v3(&quote, &collaterals, current_time)
+        verify_quote(&quote, &collaterals, current_time)
             .unwrap()
             .to_bytes()
             .as_slice(),
